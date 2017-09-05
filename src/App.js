57a3ef9bbed011627858a599547ff66d6df82e76
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2017-09-05 17:14:31 +0900

    got everything working and updated the README.md file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+const marked = require('marked')
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      markup: 'Heading\n=======\n\nSub-heading\n-----------\n' +
+      '\n### Another deeper heading\n \nParagraphs are separated by a blank line.' +
+      '\n\nLeave 2 spaces at the end of a line to do a line break.\n\nText attributes:  \n' +
+      ' *italic*, **bold**, `monospace`, ~~strikethrough~~\n\n' +
+      '> Block quotes are  \n> written like this.\n>\n> They can span multiple lines, \n> too.' +
+      '\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n' +
+      '  2. oranges\n  3. pears\n\nThe rain---not the reign---in Spain.\n\n' +
+      '*[Kris Koishigawa](https://www.freecodecamp.org/scissorsneedfoodtoo)*',
+    }
+
+    // for testing
+    // console.log(marked(this.state.textarea));
+    this.handleChange = this.handleChange.bind(this);
+  } // end Constructor
+
+  handleChange(event) {
+    const updatedTextarea = event.target.value
+
+    this.setState({
+      markup: updatedTextarea,
+    });
+  }
+
+  renderHTML() {
+    const rawMarkup = marked(this.state.markup, {sanitized: true})
+
+    return {__html: rawMarkup}
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="App container">
+        <div className="col">
+          <div className="col-sm-6">
+            <textarea className="form-control" rows="22" value={this.state.markup} onChange={this.handleChange}/>
+          </div>
+          <div className="col-sm-6">
+            <div className="output" dangerouslySetInnerHTML={this.renderHTML()} />
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </div> // end App
     );
   }
 }
